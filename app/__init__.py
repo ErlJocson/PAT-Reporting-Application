7dad0a2543f6c258d9441d4b7105b2b6d9e44ce4
erl <jocsonerl@gmail.com>
2026-03-09 19:43:17 +0800

    added transformer file

--- app/__init__.py
+++ app/__init__.py
@@ -16,6 +16,7 @@ from app.variables import (
     segment_to_drop_columns,
     dump_to_drop_columns
 )
+from app.transformers import name_and_card_reference_transformers
 import os
 import pandas as pd
 import numpy as np
@@ -459,19 +460,13 @@ def start(main_data_dump, output_directory) -> None:
             return 0
         return 1
 
-    name_reference_df =  pd.read_excel(main_data_dump / 'Name References.xlsx', sheet_name = 'DUMP')
-    ccp_name_reference_df =  pd.read_excel(main_data_dump / 'Name References.xlsx', sheet_name = 'CCP Name')
-    card_type_refereces_for_vg_toolkit_df = pd.read_excel(main_data_dump / 'Card References.xlsx', sheet_name = 'Sheet1', usecols = ['PAT Card List', 'Referrence'])
+    name_and_card_reference_transformed_data = name_and_card_reference_transformers(main_data_dump = main_data_dump)
 
-    team_leader_reference_df = name_reference_df[['Team Leader', 'Team Leader New Name']].copy().dropna()
-    monitored_by_reference_df = name_reference_df[['Monitored By', 'Monitored By New Name']].copy().dropna()
-    manager_reference_df = name_reference_df[['Manager', 'Manager New Name']].copy().dropna()
-
-    team_leader_reference_df = dict(zip(team_leader_reference_df['Team Leader'], team_leader_reference_df['Team Leader New Name']))
-    monitored_by_reference_df = dict(zip(monitored_by_reference_df['Monitored By'], monitored_by_reference_df['Monitored By New Name']))
-    manager_reference_df = dict(zip(manager_reference_df['Manager'], manager_reference_df['Manager New Name']))
-    ccp_name_reference_df = dict(zip(ccp_name_reference_df['Employee ID'], ccp_name_reference_df['CCP Name']))
-    card_type_refereces_for_vg_toolkit_df = dict(zip(card_type_refereces_for_vg_toolkit_df['PAT Card List'], card_type_refereces_for_vg_toolkit_df['Referrence']))
+    team_leader_reference_df = name_and_card_reference_transformed_data['team_leader_reference_df']
+    monitored_by_reference_df = name_and_card_reference_transformed_data['monitored_by_reference_df']
+    manager_reference_df = name_and_card_reference_transformed_data['manager_reference_df']
+    ccp_name_reference_df = name_and_card_reference_transformed_data['ccp_name_reference_df']
+    card_type_refereces_for_vg_toolkit_df = name_and_card_reference_transformed_data['card_type_refereces_for_vg_toolkit_df']
 
     def get_ccp_name(r):
         return ccp_name_reference_df.get(r)
